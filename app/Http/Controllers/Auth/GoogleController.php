5f5da3c44125b9d26c35c894102d7d4ae2178613
saadkhan3977 <xaadkhan43@gmail.com>
2024-12-05 15:58:01 +0500

    google login

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Google_Client;
 
 class GoogleController extends Controller
 {
